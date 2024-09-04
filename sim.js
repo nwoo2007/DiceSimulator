23b59eb078381315846ae86312d7e4c1ebd60033
nwoo2007 <n.woo2@share.epsb.ca>
2024-09-04 09:01:41 -0600

    sim.js

--- sim.js
+++ sim.js
@@ -19,8 +19,8 @@ let angle2 = 0;
 let timer = setInterval(animateDice, 200);
 
 // HTML Variables
-let numDice = +document.getElementById("num-dice");
-let numSides = +document.getElementById("num-sides").value;
+let numDiceEl = document.getElementById("num-dice");
+let numSidesEl = document.getElementById("num-sides");
 
 let selectEl = document.getElementById("roll-menu");
 let rollBtn = document.getElementById("roll-btn");
@@ -44,45 +44,128 @@ function rollDice() {
 
   // Roll Dice Once
   if (selectEl.value == "roll-1") {
-    // Create random numbers to represent the dice roll
-    let dice1 = `Math.random() * ${numSides} + 1`;
-    // Math.random() * 6 + 1; // 1 - 6.999...
-    let dice2 = Math.random() * 6 + 1;
-    dice1 = Math.floor(dice1); // 1 - 6
-    dice2 = Math.floor(dice2);
-    console.log(dice1);
-    console.log(dice2);
-
-    // display the right iamges
-    dice1Img.src = `images/${dice1}.png`;
-    dice2Img.src = `images/${dice2}.png`;
-
-    // update the history box
-    historyBox.innerHTML += `<span> ${dice1}, ${dice2} </span>`;
+    // One Dice
+    if (+numDiceEl.value == 1) {
+      let dice1 = Math.random() * +numSidesEl.value + 1;
+      dice1 = Math.floor(dice1);
+      dice1Img.src = `images/${dice1}.png`;
+      dice2Img.src = `images/0.png`;
+      historyBox.innerHTML += `<span> ${dice1}</span>`;
+      // Two Dice
+    } else if (+numDiceEl.value == 2) {
+      // Create random numbers to represent the dice roll
+      let dice1 = Math.random() * +numSidesEl.value + 1;
+      // Math.random() * 6 + 1; // 1 - 6.999...
+      let dice2 = Math.random() * +numSidesEl.value + 1;
+      dice1 = Math.floor(dice1); // rounds to the nearest whole #
+      dice2 = Math.floor(dice2);
+      console.log(dice1);
+      console.log(dice2);
+      // display the right iamges
+      dice1Img.src = `images/${dice1}.png`;
+      dice2Img.src = `images/${dice2}.png`;
+      // update the history box
+      historyBox.innerHTML += `<span> ${dice1}, ${dice2} </span>`;
+      // Three Dice
+    } else if (+numDiceEl.value == 3) {
+      let dice1 = Math.random() * +numSidesEl.value + 1;
+      let dice2 = Math.random() * +numSidesEl.value + 1;
+      let dice3 = Math.random() * +numSidesEl.value + 1;
+      dice1 = Math.floor(dice1);
+      dice2 = Math.floor(dice2);
+      dice3 = Math.floor(dice3);
+      // display the right images
+      dice1Img.src = `images/${dice1}.png`;
+      dice2Img.src = `images/${dice2}.png`;
+      // update the history box
+      historyBox.innerHTML += `<span> ${dice1}, ${dice2}, ${dice3} </span>`;
+      // Four Dice
+    } else if (+numDiceEl.value == 4) {
+      let dice1 = Math.random() * +numSidesEl.value + 1;
+      let dice2 = Math.random() * +numSidesEl.value + 1;
+      let dice3 = Math.random() * +numSidesEl.value + 1;
+      let dice4 = Math.random() * +numSidesEl.value + 1;
+      dice1 = Math.floor(dice1);
+      dice2 = Math.floor(dice2);
+      dice3 = Math.floor(dice3);
+      dice4 = Math.floor(dice4);
+      // display the right images
+      dice1Img.src = `images/${dice1}.png`;
+      dice2Img.src = `images/${dice2}.png`;
+      // update the history box
+      historyBox.innerHTML += `<span> ${dice1}, ${dice2}, ${dice3}, ${dice4} </span>`;
+    } else {
+      alert("ERROR. Check inputs.");
+    }
   }
 
   // Roll Dice 5 Times
   if (selectEl.value == "roll-5") {
     let count = 0;
-    while (count < 5) {
-      let dice1 = Math.random() * 6 + 1;
-      let dice2 = Math.random() * 6 + 1;
-      dice1 = Math.floor(dice1);
-      dice2 = Math.floor(dice2);
-      dice1Img.src = `images/${dice1}.png`;
-      dice2Img.src = `images/${dice2}.png`;
-      historyBox.innerHTML += `<span> ${dice1}, ${dice2} </span>`;
-      count += 1;
+    // One Dice
+    if (+numDiceEl.value == 1) {
+      while (count < 5) {
+        let dice1 = Math.random() * +numSidesEl.value + 1;
+        dice1 = Math.floor(dice1);
+        dice1Img.src = `images/${dice1}.png`;
+        dice2Img.src = `images/0.png`;
+        historyBox.innerHTML += `<span> ${dice1}</span>`;
+        count += 1;
+      }
+      // Two Dice
+    } else if (+numDiceEl.value == 2) {
+      while (count < 5) {
+        let dice1 = Math.random() * +numSidesEl.value + 1;
+        let dice2 = Math.random() * +numSidesEl.value + 1;
+        dice1 = Math.floor(dice1);
+        dice2 = Math.floor(dice2);
+        dice1Img.src = `images/${dice1}.png`;
+        dice2Img.src = `images/${dice2}.png`;
+        historyBox.innerHTML += `<span> ${dice1}, ${dice2} </span>`;
+        count += 1;
+      }
+      // Three Dice
+    } else if (+numDiceEl.value == 3) {
+      while (count < 5) {
+        let dice1 = Math.random() * +numSidesEl.value + 1;
+        let dice2 = Math.random() * +numSidesEl.value + 1;
+        let dice3 = Math.random() * +numSidesEl.value + 1;
+        dice1 = Math.floor(dice1);
+        dice2 = Math.floor(dice2);
+        dice3 = Math.floor(dice3);
+        dice1Img.src = `images/${dice1}.png`;
+        dice2Img.src = `images/${dice2}.png`;
+        historyBox.innerHTML += `<span> ${dice1}, ${dice2}, ${dice3} </span>`;
+        count += 1;
+      }
+      // Four Dice
+    } else if (+numDiceEl.value == 4) {
+      while (count < 5) {
+        let dice1 = Math.random() * +numSidesEl.value + 1;
+        let dice2 = Math.random() * +numSidesEl.value + 1;
+        let dice3 = Math.random() * +numSidesEl.value + 1;
+        let dice4 = Math.random() * +numSidesEl.value + 1;
+        dice1 = Math.floor(dice1);
+        dice2 = Math.floor(dice2);
+        dice3 = Math.floor(dice3);
+        dice4 = Math.floor(dice4);
+        dice1Img.src = `images/${dice1}.png`;
+        dice2Img.src = `images/${dice2}.png`;
+        historyBox.innerHTML += `<span> ${dice1}, ${dice2}, ${dice3}, ${dice4} </span>`;
+        count += 1;
+      }
+    } else {
+      alert("ERROR. Check inputs.");
     }
   }
 
-  // Roll Dice 10 times
+  // Roll Dice X times
   if (selectEl.value == "roll-x") {
     let userX = prompt("How many times do you want to roll the dice?");
     let count = 0;
     while (count < userX) {
-      let dice1 = Math.random() * 6 + 1; // 1 - 6.999...
-      let dice2 = Math.random() * 6 + 1;
+      let dice1 = Math.random() * +numSidesEl.value + 1;
+      let dice2 = Math.random() * +numSidesEl.value + 1;
       dice1 = Math.floor(dice1);
       dice2 = Math.floor(dice2);
       dice1Img.src = `images/${dice1}.png`;
@@ -92,25 +175,38 @@ function rollDice() {
     }
   }
 
+  // // Force infiinite loop, requires a break condition;
+  // let x = 1;
+  // while(true) {
+  //   x++;
+  //   if (x == 10) {
+  //     break;
+  //   }
+  // }
+
   // Roll Dice Until Snake Eyes
   if (selectEl.value == "roll-snake") {
-    let dice1 = 0;
-    let dice2 = 0;
-    while (true) {
-      if (dice1 == 1 && dice2 == 1) {
-        dice1Img.src = `images/1.png`;
-        dice2Img.src = `images/1.png`;
-        break;
+    if (+numDiceEl.value == 2) {
+      let dice1 = 0;
+      let dice2 = 0;
+      while (true) {
+        if (dice1 == 1 && dice2 == 1) {
+          dice1Img.src = `images/1.png`;
+          dice2Img.src = `images/1.png`;
+          break;
+        }
+        dice1 = Math.random() * +numSidesEl.value + 1;
+        dice2 = Math.random() * +numSidesEl.value + 1;
+        dice1 = Math.floor(dice1); // 1 - 6
+        dice2 = Math.floor(dice2);
+        console.log(dice1);
+        console.log(dice2);
+        dice1Img.src = `images/${dice1}.png`;
+        dice2Img.src = `images/${dice2}.png`;
+        historyBox.innerHTML += `<span> ${dice1}, ${dice2} </span>`;
       }
-      dice1 = Math.random() * 6 + 1; // 1 - 6.999...
-      dice2 = Math.random() * 6 + 1;
-      dice1 = Math.floor(dice1); // 1 - 6
-      dice2 = Math.floor(dice2);
-      console.log(dice1);
-      console.log(dice2);
-      dice1Img.src = `images/${dice1}.png`;
-      dice2Img.src = `images/${dice2}.png`;
-      historyBox.innerHTML += `<span> ${dice1}, ${dice2} </span>`;
+    } else {
+      alert("ERROR. There must be only 2 dice.");
     }
   }
 }
@@ -142,3 +238,19 @@ function resetDice() {
   timer = setInterval(animateDice, 200);
   historyBox.innerHTML = `<h3>Dice Roll</h3>`;
 }
+
+// // One Dice
+// if (+numDiceEl.value == 1) {
+
+//   // Two Dice
+// } else if (+numDiceEl.value == 2) {
+
+//   // Three Dice
+// } else if (+numDiceEl.value == 3) {
+
+//   // Four Dice
+// } else if (+numDiceEl.value == 4) {
+
+// } else {
+//   alert("ERROR. Check inputs.");
+// }
